63c8bd0d3971b5220ff880f61ab8a4b473667415
Branimir Medvedec <94074954+BranimirMedvedec@users.noreply.github.com>
2024-06-09 19:48:16 +0200

    process complete

--- camunda/client.ts
+++ camunda/client.ts
@@ -1,5 +1,5 @@
 import { Client, logger, Variables } from "camunda-external-task-client-js"
-import { checkCategoryName } from "@/data/categories"
+import { checkCategoryName, setCategory } from "@/data/categories"
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
@@ -16,14 +16,12 @@ client.subscribe("check_name", async function ({ task, taskService }) {
 	const name = task.variables.get("name")
 	const exists = await checkCategoryName(name)
 	const unique = !exists
-	console.log("Name is unique: ", unique)
 
 	try {
 		console.log("Setting uniqueName variable...")
 		const processVariables = new Variables()
 		processVariables.set("uniqueName", unique)
-
-		console.log("Completing task with uniqueName variable...")
+		console.log("uniqueName set to: ", unique)
 		await taskService.complete(task, processVariables)
 		console.log("Task completed successfully.")
 	} catch (error) {
@@ -32,12 +30,17 @@ client.subscribe("check_name", async function ({ task, taskService }) {
 })
 
 client.subscribe("add_category", async function ({ task, taskService }) {
-	const variables = task.variables.getAll()
-	console.log("Add category task:")
-	console.log("Received task: ", task)
-	console.log("with variables: ", variables)
-	// const caller_id = task.variables.get("name");
-	// const outcome = handle_call_with_id(caller_id);
-	// complete the task
-	await taskService.complete(task)
+	const name = task.variables.get("name")
+	const description = task.variables.get("description")
+	const category = { name, description }
+
+	try {
+		console.log("Setting category...")
+		await setCategory(category)
+		console.log("Category added successfully.")
+		await taskService.complete(task)
+		console.log("Task completed successfully.")
+	} catch (error) {
+		console.error("Failed to complete task:", error)
+	}
 })
